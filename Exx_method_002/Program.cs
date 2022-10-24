8ba2bfef06066ef7284cd2377338421d2f66496d
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-10-24 20:39:43 +0300

    именные аргументы

--- Exx_method_002/Program.cs
+++ Exx_method_002/Program.cs
@@ -4,3 +4,16 @@
 }
 
 Method2("этот текст передается в аргумент msg типа string. Дале в теле метода выполняются операции с данными переданными в аргумент msg");
+
+void Method21 (string msg, string msg1, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine (msg);
+        Console.WriteLine (msg1);
+        i++;
+    }
+}
+
+Method21 (msg:"именные аргументы используются для передачи значений в аргументы с соответствующими именами при вызове метода.", msg1:" пример msg: текст, count: число", count: 3 );
