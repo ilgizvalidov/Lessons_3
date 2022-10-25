3a2b196985a144d5065547b1c55d59b2b1174293
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-10-25 21:03:27 +0300

    этот тип метода принимает и возвращает полученные значения аргументов. возвращается тот тип который был получен и определен в определении метода

---
+++ Exx_Method_004/Program.cs
@@ -0,0 +1,15 @@
+﻿string Method4 (int count, string text)
+{
+    int i = 0;
+    string result = string.Empty;
+    
+        while ( i < count)
+        {
+            result = result + text;
+            i++;
+        }
+    return result;
+}
+
+string res = Method4 (10, " этот текс будет выведен 10 раз ");
+Console.WriteLine (res);
