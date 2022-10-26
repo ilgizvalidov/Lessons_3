b4c5f30d742f28793b81a742f2ddd67bb98fbc94
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-10-26 19:57:43 +0300

    замена символов в строке

---
+++ Exx_Replace/Program.cs
@@ -0,0 +1,18 @@
+﻿string text = "какой-то текст, который надо обработать";
+
+string Replace (string text1, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int lenght = text1.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if (text1[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text1[i]}";
+
+    }
+
+    return result;
+} 
+
+string newText = Replace (text, 'к', 'ы');
+Console.WriteLine (newText);  
